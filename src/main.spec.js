4c561aecae82d35bd2b15329b41f004aff6a0e6a
Lucas Henrique <luketevl@gmail.com>
2017-12-19 01:04:51 -0200

    cli

---
+++ src/main.spec.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+
+const exec = require('child_process').exec;
+
+const btcConverter = './src/main.js';
+
+const pkg = require('../package.json');
+
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the descrition when  btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes(pkg.description)).to.be.true;
+      done();
+    });
+  });
+});
